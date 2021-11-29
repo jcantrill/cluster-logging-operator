77053cbc1a98dba3c76e46b25d790d2c6f0a085b
Jeff Cantrill <jcantril@redhat.com>
2021-11-29 17:31:14 -0500

    wip

--- test/framework/functional/vector/deploy.go
+++ test/framework/functional/vector/deploy.go
@@ -1,6 +1,7 @@
 package vector
 
 import (
+	"github.com/ViaQ/logerr/log"
 	"github.com/openshift/cluster-logging-operator/internal/constants"
 	"github.com/openshift/cluster-logging-operator/internal/runtime"
 	"github.com/openshift/cluster-logging-operator/internal/utils"
@@ -12,11 +13,22 @@ type VectorCollector struct {
 }
 
 func (c *VectorCollector) DeployConfigMapForConfig(name, config, clfYaml string) error {
+	log.V(2).Info("Creating config configmap")
+	configmap := runtime.NewConfigMap(c.NS.Name, name, map[string]string{})
+	runtime.NewConfigMapBuilder(configmap).
+		Add("vector.toml", config).
+		Add("clfyaml", clfYaml)
+	if err := c.Create(configmap); err != nil {
+		return err
+	}
 	return nil
 }
 
 func (c *VectorCollector) BuildCollectorContainer(b *runtime.ContainerBuilder, nodeName string) *runtime.ContainerBuilder {
-	return b
+	return b.AddEnvVar("LOG_LEVEL", "debug").
+		AddEnvVarFromFieldRef("POD_IP", "status.podIP").
+		AddEnvVar("NODE_NAME", nodeName).
+		AddVolumeMount("config", "/etc/vector", "", true)
 }
 
 func (c *VectorCollector) IsStarted(logs string) bool {
