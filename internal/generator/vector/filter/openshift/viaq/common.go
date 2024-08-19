fe45881359cbb43f198eff20db91716aba357219
Jeff Cantrill <jcantril@redhat.com>
2024-08-19 15:00:39 -0400

    wip

--- internal/generator/vector/filter/openshift/viaq/common.go
+++ internal/generator/vector/filter/openshift/viaq/common.go
@@ -4,6 +4,6 @@ const (
 	ClusterID            = `.openshift.cluster_id = "${OPENSHIFT_CLUSTER_ID:-}"`
 	SetTimestampField    = `ts = del(._internal.timestamp); if !exists(."@timestamp") {."@timestamp" = ts}`
 	Message              = `.message = ._internal.message`
-	SetOpenShift         = `if exists(._internal.openshift) {.openshift = ._internal.openshift}}`
+	SetOpenShift         = `if exists(._internal.openshift) {.openshift = ._internal.openshift}`
 	VRLOpenShiftSequence = `.openshift.sequence = to_unix_timestamp(now(), unit: "nanoseconds")`
 )
