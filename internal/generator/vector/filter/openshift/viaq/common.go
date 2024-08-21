9b24fb444ed1fcb397fe7565dba9fdb52f6ac99d
Jeff Cantrill <jcantril@redhat.com>
2024-08-21 15:00:39 -0400

    make audit work

--- internal/generator/vector/filter/openshift/viaq/common.go
+++ internal/generator/vector/filter/openshift/viaq/common.go
@@ -1,9 +1,9 @@
 package viaq
 
 const (
-	ClusterID            = `.openshift.cluster_id = "${OPENSHIFT_CLUSTER_ID:-}"`
-	SetTimestampField    = `ts = del(._internal.timestamp); if !exists(."@timestamp") {."@timestamp" = ts}`
-	Message              = `.message = ._internal.message`
-	SetOpenShift         = `if exists(._internal.openshift) {.openshift = ._internal.openshift}`
-	VRLOpenShiftSequence = `.openshift.sequence = to_unix_timestamp(now(), unit: "nanoseconds")`
+	SetClusterID         = `._internal.openshift.cluster_id = "${OPENSHIFT_CLUSTER_ID:-}"`
+	SetTimestampField    = `ts = del(._internal.timestamp); if !exists(._internal."@timestamp") {._internal."@timestamp" = ts}`
+	SetMessageOnRoot     = `.message = del(._internal.message)`
+	SetOpenShiftOnRoot   = `if exists(._internal.openshift) {.openshift = ._internal.openshift}`
+	SetOpenShiftSequence = `._internal.openshift.sequence = to_unix_timestamp(now(), unit: "nanoseconds")`
 )
