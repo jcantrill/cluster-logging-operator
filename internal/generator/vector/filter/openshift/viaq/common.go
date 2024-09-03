6fa1045a8b3b759dccbfa1e6be9440670e747d58
Jeff Cantrill <jcantril@redhat.com>
2024-09-03 17:06:15 -0400

    app and eventrouter

--- internal/generator/vector/filter/openshift/viaq/common.go
+++
@@ -1,7 +0,0 @@
-package viaq
-
-const (
-	SetMessageOnRoot     = `.message = del(._internal.message)`
-	SetOpenShiftOnRoot   = `if exists(._internal.openshift) {.openshift = ._internal.openshift}`
-	SetOpenShiftSequence = `._internal.openshift.sequence = to_unix_timestamp(now(), unit: "nanoseconds")`
-)
