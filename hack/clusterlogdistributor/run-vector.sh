8dd1ee34888d162f913481b3a883f18cb8eef0af
Jeff Cantrill <jcantril@redhat.com>
2025-11-13 13:10:10 -0500

    wip - proto manifests

---
+++ hack/clusterlogdistributor/run-vector.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -uo pipefail
+
+# The directory used for persisting Vector state, such as on-disk buffers, file checkpoints, and more.
+VECTOR_DATA_DIR=/var/lib/vector/openshift-logging/mine
+echo "Creating the directory used for persisting Vector state $VECTOR_DATA_DIR"
+mkdir -p ${VECTOR_DATA_DIR}
+
+echo "Starting Vector process..."
+exec /usr/bin/vector --config-toml /etc/vector/vector.toml
